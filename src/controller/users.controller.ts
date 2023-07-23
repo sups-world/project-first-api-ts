57a7647ba9c2a7e8b2a256c5060acf5ae61de202
Suprajna Sharma <suprasubedi11@gmail.com>
2023-07-23 11:43:54 +0545

    view all users

--- src/controller/users.controller.ts
+++ src/controller/users.controller.ts
@@ -1,8 +1,36 @@
 import express from 'express';
 
-export const viewAllUsers = (req: express.Request, res: express.Response) => {
+// interface
+interface userInfo {
+  name: string;
+  email: string;
+}
+
+// dummy database
+const dummyDb3: userInfo[] = [
+  {
+    name: 'ram',
+    email: 'ram@gmail.com',
+  },
+  {
+    name: 'shyam',
+    email: 'shyam@gmail.com',
+  },
+];
+
+export const viewAllUsers = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
   // access db, get list of all users
-  res.send('view all users');
+  try {
+    const allUsers: userInfo[] = dummyDb3.map(a => a);
+    res.send(allUsers);
+  } catch (error) {
+    console.log(error);
+    res.send('error occured here');
+  }
 };
 
 export const viewSingleUser = (req: express.Request, res: express.Response) => {

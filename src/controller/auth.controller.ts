aea6d2bc264f6fc3e95cfee5aa0f792fddee22fb
Suprajna Sharma <suprasubedi11@gmail.com>
2023-07-26 15:06:47 +0545

    sign up and login new way

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import bcrypt, { hash, compareSync } from 'bcrypt';
 import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
 import { User } from '../models/user.model';
-import { UserInfo } from '../interface/user.interface';
+// import { UserInfo } from '../interface/user.interface';
 
 // interface
 interface accountInfo {
@@ -29,99 +29,146 @@ const dummyDb2: accountInfo[] = [
 // // dummy Database
 
 // function for signup
-export const signUp = async (
+// export const signUp = async (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction,
+// ) => {
+//   // init new user
+//   //password hashing
+//   // inside req.body we get name,email,password
+//   // destructuring must be with the same names as in the request
+//   const { name, email, password } = req.body;
+
+//   //check if email is unique
+//   // TODO ::make another function called viewFirst to get emails
+//   const found = User.viewAll(a => email === a.email);
+//   if (found) {
+//     res.status(400).send('Email already used');
+//   }
+//   bcrypt
+//     .hash(password, 10)
+//     .then(hash => {
+//       const newUser1 = {
+//         name: name.toLowerCase(),
+//         email: email.toLowerCase(),
+//         password: hash,
+//       };
+//       User.addUsers(name, email, password);
+//       //  // creating a new token
+//       //  const token = jwt.sign({ newUser }, process.env.SECRET_KEY as string, {
+//       //   expiresIn: '7 days',
+//       // });
+//       // console.log(token, 'this is token');
+
+//       // // return token, no need to save token..front end stores the token
+//       // // saving token
+//       // // newUser.token = token;
+
+//       // // returning the token while console logging newUser
+//       // console.log(newUser, 'new user created successfully');
+//       // return res.status(201).json({ token: token });
+
+//       return res.end();
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       return res.status(500).send(err);
+//     });
+// };
+
+// export const loginUser = async (
+//   req: express.Request,
+//   res: express.Response,
+//   next: express.NextFunction,
+// ) => {
+//   // destructuring data from req.body
+//   const { email, password } = req.body;
+//   console.log('entered email', email);
+
+//   // find email in existing database..throw error if not found
+
+//   // try {
+//   const found = dummyDb2.find(data => data.email === email);
+//   if (found) {
+//     console.log('email exists:::');
+//     const userData: accountInfo = found;
+//     // if email found compare hashed password
+//     try {
+//       const match = await bcrypt.compare(password, userData.hashedPw);
+//       if (!match) {
+//         return res.status(401).send('wrong password'); //return garda, flow is stopped here..makes program efficient
+//       }
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send('server error');
+//     }
+
+//     // assign a jwt token
+//     // const token = jwt.encode({
+//     //   username:username,
+//     //   expire: Date.now() + (1000*60 *60) //1 hour
+
+//     // },tokenSecret);
+//     // callbackify(null,token);
+
+//     try {
+//       const token = jwt.sign({ userData }, process.env.SECRET_KEY as string, {
+//         expiresIn: '7 days',
+//       });
+//       console.log('token is:::', token);
+//       // return token
+//       return res.status(200).json({ token: token }).end();
+//       // .send(`welcome ${userData.email}`);
+//       // .send(`welcome ${userData.email}`);
+
+//       // no need to store token in db like sessions
+//     } catch (error) {
+//       console.log(error);
+//       return res.status(500).send('unable to login due to server error');
+//     }
+//   }
+// };
+
+//USING MODELS
+//signup module
+export const signup = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  // init new user
-  //password hashing
-  // inside req.body we get name,email,password
-  // destructuring must be with the same names as in the request
-  const { name, email, password } = req.body;
+  const { email, password, name } = req.body;
 
-  //check if email is unique
+  const existingUser = User.findFirst(a => a.email === email);
+  if (existingUser) {
+    res.status(400).send('email already used');
+  }
 
-  bcrypt
-    .hash(password, 10)
-    .then(hash => {
-      const newUser1 = {
-        name: name.toLowerCase(),
-        email: email.toLowerCase(),
-        password: hash,
-      };
-      User.addUsers(name, email, password);
-      //  // creating a new token
-      //  const token = jwt.sign({ newUser }, process.env.SECRET_KEY as string, {
-      //   expiresIn: '7 days',
-      // });
-      // console.log(token, 'this is token');
+  const hashedPwd = await bcrypt.hash(password, 10);
+  const user = User.add({ email, name, password: hashedPwd });
 
-      // // return token, no need to save token..front end stores the token
-      // // saving token
-      // // newUser.token = token;
-
-      // // returning the token while console logging newUser
-      // console.log(newUser, 'new user created successfully');
-      // return res.status(201).json({ token: token });
-
-      return res.end();
-    })
-    .catch(err => {
-      console.log(err);
-      return res.status(500).send(err);
-    });
+  res.status(201).send(user);
 };
 
-export const loginUser = async (
+//sign in module
+export const login = async (
   req: express.Request,
   res: express.Response,
   next: express.NextFunction,
 ) => {
-  // destructuring data from req.body
   const { email, password } = req.body;
-  console.log('entered email', email);
 
-  // find email in existing database..throw error if not found
+  const user = User.findFirst(a => a.email === email);
 
-  // try {
-  const found = dummyDb2.find(data => data.email === email);
-  if (found) {
-    console.log('email exists:::');
-    const userData: accountInfo = found;
-    // if email found compare hashed password
-    try {
-      const match = await bcrypt.compare(password, userData.hashedPw);
-      if (!match) {
-        return res.status(401).send('wrong password'); //return garda, flow is stopped here..makes program efficient
-      }
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send('server error');
-    }
-
-    // assign a jwt token
-    // const token = jwt.encode({
-    //   username:username,
-    //   expire: Date.now() + (1000*60 *60) //1 hour
-
-    // },tokenSecret);
-    // callbackify(null,token);
-
-    try {
-      const token = jwt.sign({ userData }, process.env.SECRET_KEY as string, {
-        expiresIn: '7 days',
-      });
-      console.log('token is:::', token);
-      // return token
-      return res.status(200).json({ token: token }).end();
-      // .send(`welcome ${userData.email}`);
-      // .send(`welcome ${userData.email}`);
-
-      // no need to store token in db like sessions
-    } catch (error) {
-      console.log(error);
-      return res.status(500).send('unable to login due to server error');
-    }
+  if (!user) {
+    res.status(401).send('Invalid email or password');
   }
+
+  const isPwdValid = await bcrypt.compare(password, user!.password);
+
+  if (!isPwdValid) {
+    res.status(401).send('Invalid email or password');
+  }
+
+  const token = jwt.sign({ id: user!.id }, 'SECRETKEY');
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,45 +1,109 @@
 // blueprint of class is an interface that has types only..not the operations
 import { UserInfo } from '../interface/user.interface';
 // extends used for classes, implements used for interfaces
-const data: Array<UserInfo> = [];
+const users: Array<UserInfo> = [];
 
 function uniqueId() {
   return Math.floor(Math.random() * Date.now());
 }
 export class User {
   //METHODS
-  static addUsers(name1: string, email1: string, password1: string) {
-    // check if email is unique against private data
-    const found = data.findIndex(a => email1 === a.email);
-    if (found === -1) {
-      //generate unique id
-      const id1: number = uniqueId();
+  // adding users
+  // static addUsers(name1: string, email1: string, password1: string) {
+  //   //generate unique id
+  //   const id1: number = uniqueId();
 
-      //make a new object with id,name,email,password then push into data Array
-      const people: UserInfo = {
-        id: id1,
-        name: name1,
-        email: email1,
-        password: password1,
-      };
-      data.push(people);
-      console.log(people, 'has been added');
-      return people;
-    } else {
-      console.log('username already exists');
-      return data;
+  //   //make a new object with id,name,email,password then push into data Array
+  //   const people: UserInfo = {
+  //     id: id1,
+  //     name: name1,
+  //     email: email1,
+  //     password: password1,
+  //   };
+  //   data.push(people);
+  //   console.log(people, 'has been added');
+  //   return people;
+
+  //   // return console.log(addData);
+  // }
+
+  // viewAllusers
+  // static viewAll(cb?: (user: UserInfo) => boolean): Array<UserInfo> {
+  //   if (cb) {
+  //     const filteredUsers = data.filter(cb);
+  //     return filteredUsers;
+  //   }
+  //   return data;
+  // }
+
+  //add user
+  static add(data: Omit<UserInfo, 'id'>): UserInfo {
+    let id = 1;
+    if (users.length) {
+      id = users[users.length - 1].id + 1;
     }
 
-    // return console.log(addData);
+    //create user object to be saved
+    const user = { id, ...data };
+    //add user to array ie save user
+    users.push(user);
+    //return new user
+    return user;
   }
-  static viewAll() {
-    if (data) {
-      return data;
-    } else {
-      return console.log('no data found');
+
+  //view all users
+  static findAll(cb?: (user: UserInfo) => boolean): Array<UserInfo> {
+    if (cb) {
+      //filter using callback
+      const filteredUsers = users.filter(cb);
+      return filteredUsers;
     }
+    return users;
   }
-  // static findById();
-  // static editById();
-  // static deleteByID();
+
+  //view by id
+  static findById(id: number): UserInfo | null {
+    const found = users.find(user => user.id === id);
+    if (!found) return null;
+    return found;
+  }
+
+  //findFirst
+  static findFirst(cb: (user: UserInfo) => boolean): UserInfo | null {
+    const found = users.find(cb);
+
+    if (!found) return null;
+    return found;
+  }
+
+  //edit name
+  static edit(id: number, data: Omit<UserInfo, 'id'>): UserInfo | null {
+    const foundIndex = users.findIndex(user => user.id === id);
+    if (foundIndex === -1) return null;
+
+    //create new user object
+    const user = { id, ...data };
+
+    //replace existing user by new user object
+    users[foundIndex] = user;
+
+    //return updated user
+    return user;
+  }
+
+  //delete user
+  static delete(id: number): UserInfo | null {
+    //find index of user to be deleted
+    const foundIndex = users.findIndex(user => user.id === id);
+    if (foundIndex === -1) return null;
+
+    //copy user object to be deleted for returning
+    const user = users[foundIndex];
+
+    //remove userfrom the list i.e delete user
+    users.splice(foundIndex, 1);
+
+    //return deleted user
+    return user;
+  }
 }

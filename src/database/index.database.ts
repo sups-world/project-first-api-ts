175f6056fce7f10b96d2959547255842257d075e
Suprajna Sharma <suprasubedi11@gmail.com>
2023-08-13 15:51:41 +0545

    prisma instance created

--- src/database/index.database.ts
+++ src/database/index.database.ts
@@ -2,19 +2,4 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  //all queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect();
-  })
-
-  .catch(async e => {
-    console.error(e);
-
-    await prisma.$disconnect();
-
-    process.exit(1);
-  });
+export default prisma;
